9e5e160819ccd148a865445dd9a17124142da8af
CaptnSisko <=>
2018-07-13 12:27:54 -0400

    Added 2 new gamemodes

--- lose_screen.rb
+++ lose_screen.rb
@@ -6,12 +6,19 @@ class LossScreen
     @score_font = Gosu::Font.new(window, Gosu::default_font_name, 40)
   end
 
-  def draw(winner, green, blue)
-    @score_font.draw(winner + " is the winner!", 435, 400, 0)
-    @score_font.draw("Green length: " + green, 470, 450, 0)
-    @score_font.draw("Blue length: " + blue, 470, 500, 0)
-    @score_font.draw("Click any key to continue!", 385, 550, 0)
-    @score_font.draw("By: Owen Jennings, Trevor Wong, Chris Knotek.", 250, 650, 0)
-    @score_font.draw("Music from Bensounds.com and Freesound.org!", 250, 700, 0)
+  def draw(winner, green, blue, multiplayer)
+    if multiplayer
+      @score_font.draw(winner + " is the winner!", 435, 400, 0)
+      @score_font.draw("Green length: " + green, 470, 450, 0)
+      @score_font.draw("Blue length: " + blue, 470, 500, 0)
+      @score_font.draw("Click any key to continue!", 385, 550, 0)
+      @score_font.draw("By: Owen Jennings, Trevor Wong, Chris Knotek.", 250, 650, 0)
+      @score_font.draw("Music from Bensounds.com and Freesound.org!", 250, 700, 0)
+    else
+      @score_font.draw("You lose!", 435, 300, 0)
+      @score_font.draw("Length: " + green, 450, 400, 0)
+      @score_font.draw("By: Owen Jennings, Trevor Wong, Chris Knotek.", 250, 650, 0)
+      @score_font.draw("Music from Bensounds.com and Freesound.org!", 250, 700, 0)
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -12,9 +12,9 @@ require_relative 'main_screens'
 require_relative 'lose_screen'
 
 class SnakeGame < Gosu::Window
-	MAX_APPLES = 5
-	MAX_GOLD_APPLES = 2
-	INITIAL_LENGTH = 10
+	#MAX_APPLES = 50
+	#MAX_GOLD_APPLES = 20
+	#INITIAL_LENGTH = 50
 	APPLE_LENGTHEN_AMOUNT = 3
 	END_SCREEN_DELAY = 120
 	BOOSTED_FRAMES = 18
@@ -24,18 +24,24 @@ class SnakeGame < Gosu::Window
 	BLUE = Gosu::Color.argb(0xff_00ffff)
 	WIDTH = 1200 # 80
 	HEIGHT = 750 # 50
-	MOVE_PERIOD = 8
+	#MOVE_PERIOD = 4
 
 	GAME_MUSIC = Gosu::Song.new('bensound-house.mp3')
 	END_MUSIC = Gosu::Song.new('bensound-scifi.mp3')
 
   def initialize
+		@multiplayer = false
+		@max_apples = 0
+		@max_gold_apples = 0
+		@initial_length = 0
+		@move_period
+
     super(WIDTH, HEIGHT)
     self.caption = "Snake Arena!"
     @screen = 0 # 0 = menu, 1 = game, 2 = loss
     ###@block = Block.new(1, 1, Gosu::Color.argb(0xff_808080))
-		@greenSnake = Snake.new(1, 1, INITIAL_LENGTH, GREEN)
-		@blueSnake = Snake.new(78, 48, INITIAL_LENGTH, BLUE)
+		@greenSnake = Snake.new(1, 1, @initial_length, GREEN)
+		@blueSnake = Snake.new(78, 48, @initial_length, BLUE)
 		@score_font = Gosu::Font.new(self, "Roboto", 25)
     GAME_MUSIC.play(true)
 
@@ -47,6 +53,8 @@ class SnakeGame < Gosu::Window
 		@sleepTime = 0
 
 		@winner = ""
+
+
   end
 
 
@@ -54,14 +62,14 @@ class SnakeGame < Gosu::Window
 		if @screen == 1
 			@turnable = true
 			@frame += 1
-			if @frame == MOVE_PERIOD / 2
+			if @frame == @move_period / 2
 				@greenSnake.move if @greenSnake.is_boosted?
 				@blueSnake.move if @blueSnake.is_boosted?
 				checkAppleCollisions
 			end
-			if @frame >= MOVE_PERIOD
+			if @frame >= @move_period
     		@greenSnake.move
-				@blueSnake.move
+				@blueSnake.move if @multiplayer
 				generate_apples
 				checkAppleCollisions
 				@frame = 0
@@ -73,16 +81,42 @@ class SnakeGame < Gosu::Window
 
   def button_down(id)
 		if @screen == 1
-    	@blueSnake.turn_down if id == Gosu::KbDown and @turnable
-    	@blueSnake.turn_up if id == Gosu::KbUp and @turnable
-    	@blueSnake.turn_left if id == Gosu::KbLeft and @turnable
-    	@blueSnake.turn_right if id == Gosu::KbRight and @turnable
-			@greenSnake.turn_down if id == Gosu::KbS and @turnable
-			@greenSnake.turn_up if id == Gosu::KbW and @turnable
-			@greenSnake.turn_left if id == Gosu::KbA and @turnable
-			@greenSnake.turn_right if id == Gosu::KbD and @turnable
+    	@blueSnake.turn_down if id == Gosu::KbDown
+    	@blueSnake.turn_up if id == Gosu::KbUp
+    	@blueSnake.turn_left if id == Gosu::KbLeft
+    	@blueSnake.turn_right if id == Gosu::KbRight
+			@greenSnake.turn_down if id == Gosu::KbS
+			@greenSnake.turn_up if id == Gosu::KbW
+			@greenSnake.turn_left if id == Gosu::KbA
+			@greenSnake.turn_right if id == Gosu::KbD
 		elsif @screen == 0
-			@screen = 1
+			if id == Gosu::KbS
+				@multiplayer = false
+				@max_apples = 4
+				@max_gold_apples = 1
+				@screen = 1
+				@initial_length = 10
+				@move_period = 8
+				@greenSnake = Snake.new(1, 1, @initial_length, GREEN)
+			elsif id == Gosu::KbP
+				@multiplayer = true
+				@max_apples = 4
+				@max_gold_apples = 1
+				@screen = 1
+				@initial_length = 10
+				@move_period = 8
+				@greenSnake = Snake.new(1, 1, @initial_length, GREEN)
+				@blueSnake = Snake.new(78, 48, @initial_length, BLUE)
+			elsif id == Gosu::KbB
+				@multiplayer = true
+				@max_apples = 50
+				@max_gold_apples = 20
+				@screen = 1
+				@initial_length = 50
+				@move_period = 4
+				@greenSnake = Snake.new(1, 1, @initial_length, GREEN)
+				@blueSnake = Snake.new(78, 48, @initial_length, BLUE)
+			end
 		elsif @screen == 2
 			if @sleepTime >= END_SCREEN_DELAY
 				@screen = 0
@@ -96,12 +130,13 @@ class SnakeGame < Gosu::Window
 		if @screen == 0
 			@title.draw
 			@frame = 0
-			@greenSnake = Snake.new(1, 1, INITIAL_LENGTH, GREEN)
-			@blueSnake = Snake.new(78, 48, INITIAL_LENGTH, BLUE)
+			@greenSnake = Snake.new(1, 1, @initial_length, GREEN)
+			@blueSnake = Snake.new(78, 48, @initial_length, BLUE)
 			@apples = []
+			@goldApples = []
 		elsif @screen == 1
 			@greenSnake.draw
-			@blueSnake.draw
+			@blueSnake.draw if @multiplayer
 			@apples.each do |apple|
 				apple.draw
 			end
@@ -109,18 +144,18 @@ class SnakeGame < Gosu::Window
 				apple.draw
 			end
 			@score_font.draw("Green length: " + @greenSnake.blocks.length.to_s, 15, 15, 0)
-			@score_font.draw("Blue length: " + @blueSnake.blocks.length.to_s, 1050, 700, 0)
-			if (@greenSnake.is_dead? (@blueSnake.blocks) and @blueSnake.is_dead? (@greenSnake.blocks))
+			@score_font.draw("Blue length: " + @blueSnake.blocks.length.to_s, 1050, 700, 0) if @multiplayer
+			if (@greenSnake.is_dead? (@blueSnake.blocks) and @blueSnake.is_dead? (@greenSnake.blocks) and @multiplayer)
 				@sleepTime = 0
 				@winner = "Nobody"
 				@screen = 2
 		    END_MUSIC.play(true)
-			elsif @greenSnake.is_dead? (@blueSnake.blocks)
+			elsif @greenSnake.is_dead? (@multiplayer ? @blueSnake.blocks : Array.new)
 				@sleepTime = 0
 				@winner = "Blue"
 				@screen = 2
 		    END_MUSIC.play(true)
-			elsif @blueSnake.is_dead? (@greenSnake.blocks)
+			elsif @blueSnake.is_dead? (@greenSnake.blocks) and @multiplayer
 				@sleepTime = 0
 				@winner = "Green"
 				@screen = 2
@@ -128,16 +163,16 @@ class SnakeGame < Gosu::Window
 			end
 		elsif @screen == 2
 			@greenSnake.draw
-			@blueSnake.draw
+			@blueSnake.draw if @multiplayer
 			@apples.each do |apple|
 				apple.draw
 			end
-			@loss.draw(@winner, @greenSnake.blocks.length.to_s, @blueSnake.blocks.length.to_s)
+			@loss.draw(@winner, @greenSnake.blocks.length.to_s, @blueSnake.blocks.length.to_s, @multiplayer)
 		end
 	end
 
 	def generate_apples
-		while @apples.length < MAX_APPLES
+		while @apples.length < @max_apples
 			apple = Block.new(rand(80), rand(50), RED)
 			overlap = false
 			@greenSnake.blocks.each do |block|
@@ -154,7 +189,7 @@ class SnakeGame < Gosu::Window
 			end
 			@apples.push apple if not overlap
 		end
-		while @goldApples.length < MAX_GOLD_APPLES
+		while @goldApples.length < @max_gold_apples
 			apple = Block.new(rand(80), rand(50), YELLOW)
 			overlap = false
 			@greenSnake.blocks.each do |block|
